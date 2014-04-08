afa8343a793ab52810833b20d81e7348286dd1db
Santtu Keskinen <laquendi@gmail.com>
2014-04-08 19:56:48 +0300

    Forking and pid saving start script

--- start_script.sh
+++ start_script.sh
@@ -2,4 +2,6 @@
 cd /kastelu
 fusermount -u sensors
 owfs -uall --allow_other sensor
-./kastelu --docroot . --http-address 0.0.0.0 --http-port 80 
+killall kastelu
+./kastelu --docroot . --http-address 0.0.0.0 --http-port 80 &
+echo $! > /var/run/kastelu.pid
